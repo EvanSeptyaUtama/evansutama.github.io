7f42d196930557e1effc08ee670615d82a112f38
Evan Septya Utama <evanseptya6@gmail.com>
2024-07-06 21:25:44 +0700

    first commit

---
+++ resources/views/portofolio/home.blade.php
@@ -0,0 +1,10 @@
+@extends('dashboard')
+@section('home')
+    <img src="{{ asset('/assets_portofolio/img/evan_malio.jpg') }}" alt="" data-aos="fade-in">
+
+    <div class="container d-flex flex-column align-items-center justify-content-center text-center" data-aos="fade-up"
+        data-aos-delay="100">
+        <h2>I am Evan Septya Utama</h2>
+        <p><span class="typed" data-typed-items="Back End Developer, Freelancer"></span></p>
+    </div>
+@endsection
